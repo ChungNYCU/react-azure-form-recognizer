f719f84bb935ebb6c1b3634e0eaf6cd9a76d6d49
Wei-Che Chung <wchung@seattleu.edu>
2022-08-14 18:08:51 -0700

    Delete redundant code line

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import {useState} from 'react';
-import ImageUploading from 'react-images-uploading';
 
 import './App.css';
-import GetFormRecongnizerResult from './components/GetFormRecongnizerResult';
 import FileUpload from './components/FileUpload';
-import MockData from './data/receipt_sample_json.json';
+//import MockData from './data/receipt_sample_json.json';
 
 function App() {
 
-  const data = MockData.analyzeResult.documents[0].fields;
-
-  const [images, setImages] = useState([]);
-  const maxNumber = 20;
-  const onChange = (imageList, addUpdateIndex) => {
-    //console.log(imageList[0].data_url, addUpdateIndex);
-    setImages(imageList);
-  };
+  //const data = MockData.analyzeResult.documents[0].fields;
 
   return (
     <div className="App">
       <FileUpload />
-
-      {/* <FormRecognizerReceiptInput propsData = {data}/> */}
-      {/* <GetFormRecongnizerResult /> */}
     </div>
   );
 }

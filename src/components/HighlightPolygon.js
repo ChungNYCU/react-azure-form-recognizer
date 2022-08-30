99a6b81d16600ee2ffc4d2d881b8af0636c56425
Wei-Che Chung <wchung@seattleu.edu>
2022-08-30 16:55:46 -0700

    component for generate polygon on receipt image

---
+++ src/components/HighlightPolygon.js
@@ -0,0 +1,41 @@
+import React from 'react';
+ 
+// input: objectKey(string), objectValue(object), xRatio(float), yRatio(float), key(int)
+// pass data in HighlightPolygon and get points to generate polygon.
+const HighlightPolygon = (props) => {
+    const data = props.objectValue;
+    const key = props.objectKey;
+    const xRatio = props.xRatio;
+    const yRatio = props.yRatio;
+
+    const getPoint = (pointNo) => {
+        const point = data.boundingRegions[0].polygon[pointNo].x*xRatio.toString() + ',' + data.boundingRegions[0].polygon[pointNo].y*yRatio.toString();
+        return point;
+    }
+
+    const getPoints = (a, b, c, d) => {
+        return a + ' ' + b + ' ' + c + ' ' + d;
+    }
+
+    // if key equal to Items then call HighlightPolygon component again for each item to process data, 
+    // otherwise generate polygon on image.
+    if(key === 'Items'){
+        return (
+            (data.values.map((items, index) => (
+                <HighlightPolygon objectKey={"Item"+index.toString()} objectValue={items} xRatio={xRatio} yRatio={yRatio} key={index}/>
+            )))
+        )
+    }else{
+        const points = getPoints(getPoint(0), getPoint(1), getPoint(2), getPoint(3));
+        return (
+            <div className="ImageDisplay-overlapInfo" key={key}>
+                <svg width="450" height="800">
+                    <polygon points={points} />
+                </svg>
+            </div>
+        )   
+    }
+}
+
+
+export default HighlightPolygon;
